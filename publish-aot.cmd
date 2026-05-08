6d4fd0aec957c46e6e87c5a88ee40d14ef46b2ed
xkyii <xkyii@foxmail.com>
2026-05-08 15:29:15 +0800

    Up: 调整发布脚本

--- publish-aot.cmd
+++ publish-aot.cmd
@@ -1,3 +1,4 @@
 @echo off
 call "D:\Scoop\apps\vsbuildtools2022\17.14.29\vs\Common7\Tools\VsDevCmd.bat" -arch=x64 -host_arch=x64 >nul 2>&1
 dotnet publish d:\Code\xkyii\Resty\src\Resty.Cli\Resty.Cli.csproj /p:PublishProfile=win-x64-aot /p:IlcUseEnvironmentalTools=true %*
+dotnet publish d:\Code\xkyii\Resty\src\Resty.Gui\Resty.Gui.csproj /p:PublishProfile=win-x64-aot /p:IlcUseEnvironmentalTools=true %*
